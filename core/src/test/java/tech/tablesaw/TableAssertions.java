c2f90b61f0f267c0a8d958ed1b1a9be7272bf8a6
jackie-h <10471950+jackie-h@users.noreply.github.com>
2019-11-15 15:30:40 -0500

    Move assertTableContents into a utility class for reuse

---
+++ core/src/test/java/tech/tablesaw/TableAssertions.java
@@ -0,0 +1,42 @@
+package tech.tablesaw;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import tech.tablesaw.api.Row;
+import tech.tablesaw.api.Table;
+
+public final class TableAssertions {
+
+  private TableAssertions() {}
+
+  public static void assertTableContents(Object[][] expected, Table actual) {
+    assertEquals(
+        expected.length - 1, actual.rowCount(), "Table does not have the same number of rows");
+
+    assertArrayEquals(
+        expected[0],
+        actual.columnNames().toArray(),
+        String.format(
+            "Column names are not equal.\nexpected: %s\nactual:   %s",
+            Arrays.toString(expected[0]), Arrays.toString(actual.columnNames().toArray())));
+
+    int expectedRowIndex = 1;
+    for (Row actualRow : actual) {
+      Object[] expectedRowData = expected[expectedRowIndex];
+      assertEquals(
+          expectedRowData.length,
+          actualRow.columnCount(),
+          "Row has the expected number of columns");
+
+      for (int colIndex = 0; colIndex < expectedRowData.length; colIndex++) {
+        assertEquals(
+            expectedRowData[colIndex],
+            actualRow.getObject(colIndex),
+            String.format("Value at row %d column %d not equal", expectedRowIndex - 1, colIndex));
+      }
+      expectedRowIndex++;
+    }
+  }
+}
